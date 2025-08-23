256f781d5a6bb266b7ca7841c7cd144aad6ef99f
Mohit Singh <mohiitsingh91@gmail.com>
2025-08-23 07:49:49 +0530

    Episode 01

---
+++ createApp.js
@@ -0,0 +1,19 @@
+
+// var heading = document.createElement("h1");
+// heading.innerHTML = "Hello Javascript";
+ 
+// const root = document.getElementById("root");
+// root.appendChild(heading);
+
+
+const heading = React.createElement("div",{id:"Parent"}, 
+    [React.createElement("div",{id:"child"}, 
+        [React.createElement("h1", {},"h1 tag"), React.createElement("h2", {},"h2 tag") ]
+), React.createElement("div",{id:"child"}, 
+        [React.createElement("h1", {},"h1 tag"), React.createElement("h2", {},"h2 tag") ]
+)]
+);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
