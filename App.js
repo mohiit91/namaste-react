ce6641ac0d56f172c2d3673eb5d2fcd6aa001c8b
Mohit Singh <mohiitsingh91@gmail.com>
2025-08-27 22:47:16 +0530

    createApp changed to App.js

---
+++ App.js
@@ -0,0 +1,9 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const jsxHeading = <h1> Hello guys JSX Baby </h1>
+ 
+
+ ReactDOM.createRoot(document.getElementById("root")).render(jsxHeading);
+// root.render(jsxHeading);
